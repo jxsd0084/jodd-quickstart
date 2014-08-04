b3645f95862488c077c27ed1ccf9f266aff60d03
Igor Spasić <igor@jodd.org>
2014-08-04 16:38:38 +0200

    jodd libs updated.

--- webapp1/lib/getjodd.sh
+++ webapp1/lib/getjodd.sh
@@ -1,4 +1,5 @@
-cp ~/prj/oblac/jodd/distribution/build/lib/jodd-3.4.5-SNAPSHOT.jar jodd
-cp ~/prj/oblac/jodd/jodd-joy/build/libs/jodd-joy-3.4.5-SNAPSHOT.jar jodd
+#!/bin/sh
+cp ~/prj/oblac/jodd/distribution/build/lib/jodd-3*jar jodd
+cp ~/prj/oblac/jodd/jodd-joy/build/libs/jodd-joy-3*.jar jodd
 echo "Done!"
 say -v Vicki "Done. Enjoy!"
