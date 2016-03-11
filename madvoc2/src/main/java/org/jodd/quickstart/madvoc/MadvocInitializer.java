e7da7c4ad5bfb634e0a5ab5b2ac8fb2d269df154
Igor Spasić <igor@jodd.org>
2016-03-11 11:58:09 +0100

    added madvoc2

--- madvoc2/src/main/java/org/jodd/quickstart/madvoc/MadvocInitializer.java
+++ madvoc2/src/main/java/org/jodd/quickstart/madvoc/MadvocInitializer.java
@@ -1,7 +1,31 @@
 package org.jodd.quickstart.madvoc;
 
-/**
- * Created by igor on 16/3/11.
- */
-public class MadvocInitializer {
+import jodd.madvoc.MadvocServletFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.EnumSet;
+import java.util.Set;
+
+public class MadvocInitializer implements ServletContainerInitializer {
+
+	@Override
+	public void onStartup(Set<Class<?>> c, ServletContext sc) throws ServletException {
+		System.out.println("+++ Hello Servlets 3.0");
+
+		FilterRegistration filter = sc.addFilter("madvoc", MadvocServletFilter.class);
+
+		filter.setInitParameter("madvoc.webapp", "org.jodd.quickstart.madvoc.AppWebApplication");
+
+		EnumSet NON_ASYNC_DISPATCHER_TYPES =
+			EnumSet.of(DispatcherType.FORWARD, DispatcherType.INCLUDE, DispatcherType.REQUEST);
+
+		filter.addMappingForUrlPatterns(NON_ASYNC_DISPATCHER_TYPES, true, "/*");
+
+		System.out.println("+++ MADVOC FILTER");
+	}
 }
+
